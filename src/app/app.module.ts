860793e65fe4b78ad72a665374fd941274f53d28
MzMahmud <moaz.mahmud.64@gmail.com>
2023-11-28 13:43:42 +0600

    hash router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, CounterComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
