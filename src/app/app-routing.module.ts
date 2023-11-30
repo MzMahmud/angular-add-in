4078b804d6743b160c434dc916ae59b5c3c43eb0
MzMahmud <moaz.mahmud.64@gmail.com>
2023-11-30 11:47:04 +0600

    basic setup for gist addin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CounterComponent } from './components/counter/counter.component';
 
-const routes: Routes = [
-  {
-    path: 'counter',
-    component: CounterComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
