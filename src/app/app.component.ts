f572e7b66476ad03cc3027a788902f923f1ebc6c
MzMahmud <moaz.mahmud.64@gmail.com>
2023-12-04 19:39:05 +0600

    build#8: for debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  environment: any;
+  constructor() {
+    this.environment = environment;
+  }
+}
