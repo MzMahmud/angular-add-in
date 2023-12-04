b9f6732ba3aade2ee156ba18b9df366bc1962eab
MzMahmud <moaz.mahmud.64@gmail.com>
2023-12-04 20:32:41 +0600

    fixed method

--- src/app/util/string.util.ts
+++ src/app/util/string.util.ts
@@ -1,8 +1,14 @@
 import { environment } from '../../environments/environment';
 
 export function getAbsoluteUrl(relativeUrl: string) {
-  const baseUrl = window.location.origin + environment.baseHref;
-  return new URL(relativeUrl, baseUrl).toString();
+  const base = removeLeadingAndTrailingSlash(window.location.origin);
+  const baseHref = removeLeadingAndTrailingSlash(environment.baseHref);
+  const relative = removeLeadingAndTrailingSlash(relativeUrl);
+  return `${base}/${baseHref}/${relative}`;
+}
+
+function removeLeadingAndTrailingSlash(s: string) {
+  return s.replace(RegExp('^/'), '').replace(RegExp('/$'), '');
 }
 
 export function addQueryParamToUrl(
